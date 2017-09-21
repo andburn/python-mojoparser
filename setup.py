d7c45b187b445aa70525bf236fe34a7540453199
Andrew Burnett <andrew@andburn.info>
2017-09-21 09:07:37 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
 
 setup(
-	name='MojoParser',
-	packages=['mojoparser']
+    name='mojoparser',
+    version='0.1.0',
+    author='andburn',
+    url='https://github.com/andburn/python-mojoparser',
+    packages=find_packages(exclude=('tests',)),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
 )
