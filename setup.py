b6dc69fb0db9dc523915fd8f306a7cd16af8b581
Andrew Burnett <andrew@andburn.info>
2017-10-29 19:41:11 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mojoparser',
-    version='0.1.0',
+    version='0.1.1',
     author='andburn',
     url='https://github.com/andburn/python-mojoparser',
     packages=find_packages(exclude=('tests',)),
